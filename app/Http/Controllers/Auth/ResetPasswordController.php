018fe5a8ed654db1fa3aa0984c5a3cb0dad77d92
Alfian <112760996+alfian8752@users.noreply.github.com>
2024-09-02 11:09:26 +0700

    reset password

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -2,28 +2,61 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use App\Helpers\ResponseHelper;
+use PhpParser\Node\Stmt\TryCatch;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Auth\Events\PasswordReset;
+use App\Http\Requests\ResetPasswordRequest;
+use App\Http\Requests\SendResetPasswordRequest;
 use Illuminate\Foundation\Auth\ResetsPasswords;
 
 class ResetPasswordController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Password Reset Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller is responsible for handling password reset requests
-    | and uses a simple trait to include this behavior. You're free to
-    | explore this trait and override any methods you wish to tweak.
-    |
-    */
+    public function sendEmail(SendResetPasswordRequest $request)
+    {
+        $data = $request->validated();
 
-    use ResetsPasswords;
+        $status = Password::sendResetLink(
+            $request->only('email')
+        );
 
-    /**
-     * Where to redirect users after resetting their password.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/home';
+        return $status === Password::RESET_LINK_SENT
+            ? ResponseHelper::success(null, "Link reset sudah dikirim, mohon cek email anda")
+            : ResponseHelper::error(null, "Terjadi kesalahan");
+    }
+
+
+    // return token untuk reset
+    public function resetToken($token)
+    {
+        return ResponseHelper::success(['token' => $token]);
+    }
+
+    // reset password
+    public function reset(Request $request)
+    {
+        $status = Password::reset(
+            $request->only('email', 'password', 'password_confirmation', 'token'),
+            function (User $user, string $password) {
+                $user->forceFill([
+                    'password' => Hash::make($password)
+                ])->setRememberToken(Str::random(60));
+
+                $user->save();
+
+
+                event(new PasswordReset($user));
+            }
+        );
+
+        return $status === Password::PASSWORD_RESET
+            ? ResponseHelper::success(null, "Berhasil reset password")
+            : ResponseHelper::error(null, "Reset password gagal");
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,15 @@
 <?php
 
-use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\Auth\RegisterController;
-use App\Http\Controllers\Course\CategoryController;
-use App\Http\Controllers\Course\CourseController;
-use App\Http\Controllers\Course\SubCategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Password;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\Auth\LoginController;
+use Illuminate\Auth\Notifications\ResetPassword;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Course\CourseController;
+use App\Http\Controllers\Auth\ResetPasswordController;
+use App\Http\Controllers\Course\SubCategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -31,5 +34,12 @@ Route::middleware('enable.cors')->group(function () {
 
     Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
         return $request->user();
+    });
+
+    Route::post('/forgot-password', [ResetPasswordController::class, 'sendEmail'])->middleware('guest')->name('password.email');
+
+    Route::middleware('throttle:10,1')->group(function () {
+        Route::get('password/reset/{token}', [ResetPasswordController::class, 'resetToken'])->name('password.reset');
+        Route::post('password/reset', [ResetPasswordController::class, 'reset'])->name('password.reset');
     });
 });
