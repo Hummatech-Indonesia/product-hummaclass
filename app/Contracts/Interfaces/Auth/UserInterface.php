980f63de98271a6e0960fcbad8ff968f5237a741
frendikawa <148562963+frendikawa@users.noreply.github.com>
2024-09-04 11:53:16 +0700

    add apiGetUsers

---
+++ app/Contracts/Interfaces/Auth/UserInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Contracts\Interfaces\Auth;
+
+use App\Contracts\Interfaces\Eloquent\BaseInterface;
+use App\Contracts\Interfaces\Eloquent\SearchInterface;
+use App\Contracts\Interfaces\Eloquent\StoreInterface;
+
+interface UserInterface extends SearchInterface
+{
+
+}
---
+++ app/Contracts/Repositories/Auth/UserRepository.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Contracts\Repositories\Auth;
+
+use App\Contracts\Interfaces\Auth\UserInterface;
+use App\Contracts\Repositories\BaseRepository;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserRepository extends BaseRepository implements UserInterface
+{
+    public function __construct(User $user)
+    {
+        $this->model = $user;
+    }
+    /**
+     * Method search
+     *
+     * @param Request $request [explicite description]
+     *
+     * @return mixed
+     */
+    public function search(Request $request): mixed
+    {
+        return $this->model->query()->get();
+    }
+}
