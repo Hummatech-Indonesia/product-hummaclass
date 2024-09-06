cc24236952a8c615e6c9590b720313a21c15d0ec
frendikawa <148562963+frendikawa@users.noreply.github.com>
2024-09-06 14:33:55 +0700

    fixed coursetask

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,10 +7,12 @@ use App\Contracts\Interfaces\Auth\UserInterface;
 use App\Contracts\Interfaces\Course\CategoryInterface;
 use App\Contracts\Interfaces\Course\CourseInterface;
 use App\Contracts\Interfaces\Course\CourseReviewInterface;
+use App\Contracts\Interfaces\Course\CourseTaskInterface;
 use App\Contracts\Interfaces\Course\ModuleInterface;
 use App\Contracts\Interfaces\Course\ModuleQuestionInterface;
 use App\Contracts\Interfaces\Course\QuizInterface;
 use App\Contracts\Interfaces\Course\SubCategoryInterface;
+use App\Contracts\Interfaces\Course\SubmissionTaskInterface;
 use App\Contracts\Interfaces\Course\SubModuleInterface;
 use App\Contracts\Interfaces\RegisterInterface;
 use App\Contracts\Repositories\Auth\ProfileRepository;
@@ -18,10 +20,12 @@ use App\Contracts\Repositories\Auth\UserRepository;
 use App\Contracts\Repositories\Course\CategoryRepository;
 use App\Contracts\Repositories\Course\CourseRepository;
 use App\Contracts\Repositories\Course\CourseReviewRepository;
+use App\Contracts\Repositories\Course\CourseTaskRepository;
 use App\Contracts\Repositories\Course\ModuleQuestionRepository;
 use App\Contracts\Repositories\Course\ModuleRepository;
 use App\Contracts\Repositories\Course\QuizRepository;
 use App\Contracts\Repositories\Course\SubCategoryRepository;
+use App\Contracts\Repositories\Course\SubmissionTaskRepository;
 use App\Contracts\Repositories\Course\SubModuleRepository;
 use App\Contracts\Repositories\RegisterRepository;
 use Illuminate\Support\ServiceProvider;
@@ -40,7 +44,9 @@ class AppServiceProvider extends ServiceProvider
         SubModuleInterface::class => SubModuleRepository::class,
         UserInterface::class => UserRepository::class,
         ModuleQuestionInterface::class => ModuleQuestionRepository::class,
-        QuizInterface::class => QuizRepository::class
+        QuizInterface::class => QuizRepository::class,
+        CourseTaskInterface::class => CourseTaskRepository::class,
+        SubmissionTaskInterface::class => SubmissionTaskRepository::class
     ];
 
     /**
