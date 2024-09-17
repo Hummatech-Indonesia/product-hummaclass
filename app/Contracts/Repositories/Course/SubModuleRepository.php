b8d965a1140d5b5c29e7b8d7f5a767bb9e24284d
mohamadarif03 <pecintarealmadrid2@gmail.com>
2024-09-17 13:29:29 +0700

    Update SubModuleRepository.php

--- app/Contracts/Repositories/Course/SubModuleRepository.php
+++ app/Contracts/Repositories/Course/SubModuleRepository.php
@@ -61,17 +61,7 @@ class SubModuleRepository extends BaseRepository implements SubModuleInterface
     {
         return $this->model->query()->findOrFail($id);
     }
-    /**
-     * Method showWithSlug
-     *
-     * @param string $slug [explicite description]
-     *
-     * @return mixed
-     */
-    public function showWithSlug(string $slug): mixed
-    {
-        return $this->model->query()->where(['slug' => $slug])->firstOrFail();
-    }
+
     /**
      * Method update
      *
