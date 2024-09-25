48fbf947d22db89fda2ec1d22c7a28f78b04d0ed
frendikawa <148562963+frendikawa@users.noreply.github.com>
2024-09-25 14:20:01 +0700

    Update EventInterface.php

--- app/Contracts/Interfaces/EventInterface.php
+++ app/Contracts/Interfaces/EventInterface.php
@@ -8,10 +8,11 @@ use App\Contracts\Interfaces\Eloquent\DeleteInterface;
 use App\Contracts\Interfaces\Eloquent\GetInterface;
 use App\Contracts\Interfaces\Eloquent\SearchInterface;
 use App\Contracts\Interfaces\Eloquent\ShowInterface;
+use App\Contracts\Interfaces\Eloquent\ShowSlugInterface;
 use App\Contracts\Interfaces\Eloquent\StoreInterface;
 use App\Contracts\Interfaces\Eloquent\UpdateInterface;
 
-interface EventInterface extends CustomPaginationInterface,StoreInterface,ShowInterface,UpdateInterface,DeleteInterface
+interface EventInterface extends CustomPaginationInterface,StoreInterface,ShowInterface,ShowSlugInterface,UpdateInterface,DeleteInterface
 {
 
 }
