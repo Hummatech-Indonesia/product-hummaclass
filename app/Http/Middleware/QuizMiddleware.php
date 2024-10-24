6332f97cfc33606ed61fc53031598b90fa7358fa
mohamadarif03 <mohamadarif5392@gmail.com>
2024-10-24 13:32:21 +0700

    Update QuizMiddleware.php

--- app/Http/Middleware/QuizMiddleware.php
+++ app/Http/Middleware/QuizMiddleware.php
@@ -27,8 +27,7 @@ class QuizMiddleware
         $subModule = SubModule::query()->where('module_id', $module->id)->orderBy('step', 'desc')->firstOrFail();
         $lasStep = $subModule->step;
         $userCourse = UserCourse::query()->where('user_id', auth()->user()->id)->where('course_id', $module->course_id)->firstOrFail();
-
-        if ($userCourse->subModule->module->step > $module->step || $userCourse->subModule->step = $lasStep) {
+        if ($userCourse->subModule->module->step > $module->step && $userCourse->subModule->step == $lasStep) {
             return ResponseHelper::error(null, "Selesaikan Materi Sebelumnya", 403);
         }
         return $next($request);
