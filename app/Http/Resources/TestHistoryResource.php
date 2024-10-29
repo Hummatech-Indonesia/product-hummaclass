18a5706c14a36bb6e496d221a2020ad18a29e470
frendikawa <148562963+frendikawa@users.noreply.github.com>
2024-10-29 10:20:37 +0700

    Update TestHistoryResource.php

--- app/Http/Resources/TestHistoryResource.php
+++ app/Http/Resources/TestHistoryResource.php
@@ -22,7 +22,8 @@ class TestHistoryResource extends JsonResource
             'module_question_ids' => $this->module_question_id,
             'answer' => $this->answer,
             'score' => $this->score,
-            'test_type' => $this->test_type
+            'test_type' => $this->test_type,
+            'created_at' => $this->created_at->format('j F Y H:i'),
         ];
     }
 }
