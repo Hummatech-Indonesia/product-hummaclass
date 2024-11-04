fa08512d89329e5398a1d9be347d8b28aed5ae3d
= <=>
2024-11-04 15:26:39 +0700

    update

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -26,7 +26,7 @@ class UserResource extends JsonResource
             'total_courses' => $this->userCourses->count(),
             'course_reviews' => ReviewResource::collection($this->courseReviews),
             // 'total_course_completed' => $this->userCourseTests()->where('type_test', TestEnum::POSTTEST->value)->whereNotNull('score')->count(),
-            'total_certificate' => $this->userEvents()->where('has_certificated', true)->count(),
+            'total_certificate' => $this->userEvents()->where('has_certificate', true)->count(),
             'course_activities' => $this->userCourses ? UserCourseResource::collection($this->userCourses) : null,
             'event_activities' => $this->userEvents ? EventResource::collection($this->userEvents) : null,
             'address' => $this->address,
