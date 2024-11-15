d6063a4f08cb9fceb19203b19f42fd2ee2802451
mohamadarif03 <mohamadarif5392@gmail.com>
2024-11-15 15:00:16 +0700

    Update DetailCourseResource.php

--- app/Http/Resources/Course/DetailCourseResource.php
+++ app/Http/Resources/Course/DetailCourseResource.php
@@ -34,7 +34,11 @@ class DetailCourseResource extends JsonResource
         });
 
         $userCource = $this->userCourses()?->where('user_id', $user?->id)->with('subModule')->first();
-        $completed = CourcePercentaceHelper::getPercentace($userCource);
+        if ($userCource) {
+            $completed = CourcePercentaceHelper::getPercentace($userCource);
+        } else {
+            $completed = null;
+        }
 
         return [
             'id' => $this->id,
