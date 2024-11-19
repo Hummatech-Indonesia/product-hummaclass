516e277c7468ec534e75f59c0863f3d94b58741e
frendikawa <148562963+frendikawa@users.noreply.github.com>
2024-11-19 09:20:05 +0700

    Update TransactionResource.php

--- app/Http/Resources/TransactionResource.php
+++ app/Http/Resources/TransactionResource.php
@@ -17,7 +17,8 @@ class TransactionResource extends JsonResource
         return [
             'id' => $this->id,
             'user' => $this->user,  // ID user
-            'product' => $this->event ? EventResource::make($this->event) : CourseResource::make($this->course),  // ID course
+            'user_phoyo' => url('storage/' . $this->user->photo),
+            'product' => $this->event ?? CourseResource::make($this->course),  // ID course
             'invoice_id' => $this->invoice_id,  // ID invoice
             'fee_amount' => $this->fee_amount,  // Biaya yang ditentukan
             'amount' => $this->amount,  // Jumlah pembayaran
