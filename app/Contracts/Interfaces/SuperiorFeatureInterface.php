ad05095d9c5d3a7cb031dbeebbbc54bf6a07c55b
frendikawa <148562963+frendikawa@users.noreply.github.com>
2024-11-26 13:40:33 +0700

    update feature

--- app/Contracts/Interfaces/SuperiorFeatureInterface.php
+++ app/Contracts/Interfaces/SuperiorFeatureInterface.php
@@ -5,4 +5,14 @@ namespace App\Contracts\Interfaces;
 use App\Contracts\Interfaces\Eloquent\GetInterface;
 use App\Contracts\Interfaces\Eloquent\StoreInterface;
 
-interface SuperiorFeatureInterface extends GetInterface, StoreInterface {}
+interface SuperiorFeatureInterface extends GetInterface
+{
+    /**
+     * Method update
+     *
+     * @param array $data [explicite description]
+     *
+     * @return mixed
+     */
+    public function update(array $data): mixed;
+}
