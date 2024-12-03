e1b5ac3463744d5ea245551876877e62d2b4a177
mohamadarif03 <mohamadarif5392@gmail.com>
2024-12-03 15:26:35 +0700

    Database Assesment

---
+++ database/migrations/2024_12_03_141939_create_assesment_form_students_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('assesment_form_students', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('assessment_form_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+            $table->enum('value', [1, 2, 3, 4, 5]);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('assesment_form_students');
+    }
+};
