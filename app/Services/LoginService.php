c143e99eb11d159f995d03d7000e141f0e2fb831
mohamadarif03 <mohamadarif5392@gmail.com>
2025-01-06 08:40:22 +0700

    Update LoginService.php

--- app/Services/LoginService.php
+++ app/Services/LoginService.php
@@ -9,24 +9,22 @@ use Illuminate\Http\Response;
 
 class LoginService
 {
-    /**
-     * handleLogin
-     *
-     * @return mixed
-     */
     public function handleLogin(LoginRequest $request): mixed
     {
         if (auth()->attempt(['email' => $request->email, 'password' => $request->password])) {
             $roles = auth()->user()->roles->pluck('name');
             $data['user'] = auth()->user();
             $data['user']->roles = $roles;
-            $data['token'] =  auth()->user()->createToken('auth_token')->plainTextToken;
+            $data['token'] = auth()->user()->createToken('auth_token')->plainTextToken;
+
             if ($request->remember) {
-                $cookie = cookie('remember_me', true, 43200);
+                $cookie = cookie('remember_me', true, 525600);
                 return ResponseHelper::success($data, trans('auth.success'))->cookie($cookie);
             }
+
             return ResponseHelper::success($data, trans('auth.success'));
         }
+
         return ResponseHelper::error(null, trans('auth.failed'), Response::HTTP_BAD_REQUEST);
     }
 }
